45c73e5ee06c80f3774995f58cad0911c623cd24
Yasmine <yasmine.benhsine03@gmail.com>
2024-05-13 15:47:48 +0100

    added

--- src/main/java/com/example/hotels/Entities/Chambre.java
+++ src/main/java/com/example/hotels/Entities/Chambre.java
@@ -29,16 +29,20 @@ public class Chambre {
     @ManyToOne
     private Hotel hotel;
 
+    // No-argument constructor
+    public Chambre() {
+    }
+
+    // Constructor
+    public Chambre(int id_room, String description, TypeRoom typeroom, boolean disponible) {
+        this.id_room = id_room;
+        this.description = description;
+        this.typeroom = typeroom;
+        this.disponible = disponible;
+    }
+
+    // Getter for 'disponible'
     public boolean getDisponible() {
         return disponible;
     }
-    public Chambre(  int id_room,String description,
-                     TypeRoom typeroom,
-                     boolean disponible) {
-        this.id_room=id_room;
-        this.description=description;
-        this.typeroom=typeroom;
-        this.disponible=disponible;
-    }
 }
-
